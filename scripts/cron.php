613014ad67454c4653f3f9357a304756ad91b633
kiang <kiange@gmail.com>
2020-02-06 07:47:01 +0800

    hook a post request

--- scripts/cron.php
+++ scripts/cron.php
@@ -12,3 +12,9 @@ exec("cd {$rootPath} && /usr/bin/git add -A");
 exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
 
 exec("cd {$rootPath} && /usr/bin/git push origin master");
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://mask.goodideas-studio.com/sync');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: 0'));
+curl_exec($ch);
