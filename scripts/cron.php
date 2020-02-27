84a454ebcd233ec89e9be4579d4b0bdc737efaec
kiang <kiange@gmail.com>
2020-02-27 20:36:16 +0800

    move cron lock to cron.php

--- scripts/cron.php
+++ scripts/cron.php
@@ -1,4 +1,11 @@
 <?php
+$cronLock = __DIR__ . '/cron_lock';
+if(file_exists($cronLock)) {
+    //previous cron is still executing, don't push it~~~
+    exit();
+}
+file_put_contents($cronLock, '1');
+
 $rootPath = dirname(__DIR__);
 
 $now = date('Y-m-d H:i:s');
@@ -12,3 +19,5 @@ exec("cd {$rootPath} && /usr/bin/git add -A");
 exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
 
 exec("cd {$rootPath} && /usr/bin/git push origin master");
+
+unlink($cronLock);
